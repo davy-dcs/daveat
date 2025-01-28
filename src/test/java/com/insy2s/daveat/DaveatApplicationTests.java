584a0eb859e9a755fa965b7b9aa3a388ceec17b2
davy-dcs <davydescamps01@gmail.com>
2025-01-28 09:23:14 +0100

    Initial commit

---
+++ src/test/java/com/insy2s/daveat/DaveatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insy2s.daveat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaveatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
